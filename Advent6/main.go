a7ba6919038869f7c31370b789f8133b079f2f94
burnsci <49537762+burnsci@users.noreply.github.com>
2025-01-02 00:03:02 +0000

    modified:   Advent6/main.go

--- Advent6/main.go
+++ Advent6/main.go
@@ -1,9 +1,30 @@
 package main
 
 import (
-
+	"bufio"
+	"os"
+	"strings"
 )
 
+func check(e error){
+	if e != nil{
+		panic(e)
+	}
+}
+
 func main(){
+    file, err := os.Open("./printing.txt")
+	check(err)
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+
+    maze := [][]string{}
+
+    for scanner.Scan(){
+		row := scanner.Text()
+        mazeRow := strings.Split(row, "")
+		maze = append(maze, mazeRow)
+	}
+
     
 }
